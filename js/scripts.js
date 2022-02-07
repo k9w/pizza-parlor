11e9733698ee6a52f4439d00b8c3985d792d0c3a
Kevin Williams <kevin@k9w.org>
2022-02-06 17:25:41 -0800

    Remove old business logic. Commit first new code to UI logic without a new test.

--- js/scripts.js
+++ js/scripts.js
@@ -38,56 +38,6 @@ Pizza.prototype.priceTheWholePizza = function(sizeChosen, toppingChosen) {
 }
 
 
-// old business logic below to be rewritten
-
-Pizza.prototype.chooseSize = function() {
-    switch (sizeOptions) {
-  case ("small"):
-    sizeChosen = 12;
-    totalPrice += 12;
-    break; 
-  case ("medium"):
-    sizeChosen = 18;
-    totalPrice += 18;
-    break; 
-  case ("large"):
-    sizeChosen = 24;
-    totalPrice += 24;
-    break;
-  default:
-    break;
-  }
-}
-
-Pizza.prototype.chooseTopping = function() {
-  switch (toppingOptions) {
-  case ("cheese"):
-    toppingChosen.push("cheese");
-    totalPrice += 5;
-    break;
-  case ("pepperoni"):
-    toppingChosen.push("pepperoni");
-    totalPrice += 7;
-    break;
-  case ("artichoke"):
-    toppingChosen.push("artichoke");
-    totalPrice += 7;
-    break;
-  case ("anchovy"):
-    toppingChosen.push("anchovy");
-    totalPrice += 7;
-    break;
-  default:
-    break;
-  }
-}
-
-Pizza.prototype.orderPizza = function() {
-  function calculatePrice() {
-  }
-}
-
-
 /* User Interface Logic */
 
 let pizza = new Pizza();
@@ -98,8 +48,9 @@ $(document).ready(function() {
     $("#result").empty();
     let sizeChosen = $("input:radio[name=sizeChosen]:checked").val();
     let toppingChosen = $("input:radio[name=toppingChosen]:checked").val();
+    sizeChosen.priceThePizzaSize;
+    toppingChosen.priceThePizzaTopping;
     $("#result").show();
-    $("#result").append(orderPizza(sizeChosen, toppingChosen));
-    $("#result").append("Order placed!");
+    $("#result").append(priceTheWholePizza(sizeChosen, toppingChosen));
   });
 });
